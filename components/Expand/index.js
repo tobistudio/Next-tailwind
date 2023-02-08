be25e56ffce7ddaef6ffc593526dc70510a56697
DawnSee0823 <DawnSee0823@gmail.com>
2023-02-08 15:40:28 -0500

    feat: update My final project

--- components/Expand/index.js
+++ components/Expand/index.js
@@ -5,7 +5,7 @@ import MIcon from "../../assets/menu-icon.png";
 export default function Expand() {
 
   const [toggleMenu, setToggleMenu] = useState(false)
-  const [menu, setMenu] = useState("grid")
+  const [name, setName] = useState("Menu")
 
   return (
     <div className={`w-[357px]`}>
@@ -13,16 +13,16 @@ export default function Expand() {
             {
             toggleMenu &&
                 <div className={`flex flex-col gap-[8px] ${toggleMenu ? "h-[210px] opacity-1" : "w-0 opacity-0"} transition-all justify-around p-2 pl-[40px]`}>
-                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer'>Back to OneGrid</div>
-                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer'>KOTE CLUB</div>
-                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer'>Grid setting</div>
-                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer'>Admin</div>
+                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer' onClick={() => setName("Back to OneGrid")}>Back to OneGrid</div>
+                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer' onClick={() => setName("KOTE CLUB")}>KOTE CLUB</div>
+                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer' onClick={() => setName("Grid setting")}>Grid setting</div>
+                    <div className='text-white text-xl hover:text-[#009473] cursor-pointer' onClick={() => setName("Admin")}>Admin</div>
                 </div>
             }
-            <div className='bg-white/25 cursor-pointer rounded-[38px] p-[10px] w-full min-w-[40px] h-[40px] flex justify-start items-center' onClick={() => setToggleMenu(!toggleMenu)}>
+            <div className='bg-white/25 cursor-pointer rounded-[38px] p-[10px] w-full min-w-[40px] h-[40px] flex justify-start items-center relative' onClick={() => setToggleMenu(!toggleMenu)}>
               <img src={MIcon.src} className="w-[20px] h-[20px]" />
-              <div className='ml-[10px] text-center text-white text-lg'>Menu</div>
-              <div className="border-none text-white text-2xl ml-[225px]">X</div>
+              <div className='ml-[10px] text-center text-white text-lg whitespace-nowrap'>{ name }</div>
+              <div className="border-none text-white text-2xl absolute right-5">X</div>
             </div>
             
         </div>
--- components/Float/index.js
+++ components/Float/index.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
 import MIcon from "../../assets/menu-icon.png";
+import MenIcon from "../../assets/Rectangle 16.png";
 
 export default function Float() {
     const [toggleMenu, setToggleMenu] = useState(false)
     const [menu, setMenu] = useState("grid")
+    const [changeImg, setChangeImg] = useState(false);
 
     return (
         <div className={`${toggleMenu ? "w-[306px]" : "w-[56px]"}  transition-all pb-5`}>
             <div className={`bg-black flex flex-row p-2 rounded-[28px] "w-[306px]"`}>
-                <div className='bg-white/25 cursor-pointer rounded-[38px] p-[10px] w-[40px] min-w-[40px] h-[40px] flex justify-center align-middle' onClick={() => setToggleMenu(!toggleMenu)}>
-                    <img src={MIcon.src} className="w-full h-full" />
+                <div className='bg-white/25 cursor-pointer rounded-[38px] p-[10px] w-[40px] min-w-[40px] h-[40px] flex justify-center align-middle' 
+                    onClick={() => setToggleMenu(!toggleMenu)}>
+                    <img src={  MIcon.src} className="w-full h-full" />
                 </div>
                 {
                 toggleMenu &&
@@ -18,7 +21,13 @@ export default function Float() {
                         <div className={`${menu === "grid" && "bg-[#333285]"} text-base text-white no-underline rounded-[38px] h-full px-[16px] flex items-center transition-all cursor-pointer`} onClick={() => setMenu("grid")}>Grid</div>
                         <div className={`${menu === "updates" && "bg-[#333285]"} text-base text-white no-underline rounded-[38px] h-full px-[16px] flex items-center transition-all cursor-pointer`} onClick={() => setMenu("updates")}>Updates</div>
                         </div>
-                        <div className='bg-[#FCBD68] px-[16px] flex items-center text-base w-[76px] text-[#22222ca1] rounded-[38px]  break-keep cursor-pointer'>Log in</div>
+                        {
+                            !changeImg ?
+                            <div className='bg-[#FCBD68] px-[16px] flex items-center text-base w-[76px] text-[#22222ca1] rounded-[38px]  break-keep cursor-pointer'
+                                onClick={() => setChangeImg(!changeImg)}
+                            >Log in</div>:
+                            <img src={  MenIcon.src} onClick={() => setChangeImg(!changeImg)} className="w-[40px] h-[40px]" />
+                        }
                     </div>
                 }
             </div>
